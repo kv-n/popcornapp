6ddd832f71b74cd784879ae4b2b3e3fe50a21a52
Kevin <k.cuison@gmail.com>
2019-01-30 17:59:06 -0800

    working edit route and comment style complete

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/users')
-const Review = require('../models/reviews')
-const { getLatestMovies } = require('./api')
 
 
 router.get('/new', (req, res) => {
@@ -15,75 +13,13 @@ router.get('/new', (req, res) => {
 router.get('/', async (req, res) => {
     try {
         const allUsers = await User.find({})
-        const latestMovies = await getLatestMovies()
-        console.log(latestMovies)
         res.render("users/index", {
             users: allUsers,
             username: req.session.username,
-            latestMovies
         })
     } catch (err) {
         res.send(err)
     }
 })
-
-
-//update form
-router.get('/:id/edit', (req, res) => {
-    User.findById(req.params.id, (err, editedUser) => {
-        if (err) {
-            res.send(err)
-        } else {
-            res.render('users/edit', {
-                user: editedUser
-            })
-        }
-    })
-})
-
-//update
-router.put('/:id', (req, res) => {
-    User.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, editedUser) => {
-        if (err) {
-            res.send(err)
-        } else {
-            res.redirect('/users')
-        }
-    })
-})
-
-
-
-//delete get and route
-router.get('/:id', (req, res) => {
-    User.findById(req.params.id, (err, foundUser) => {
-        if (err) {
-            res.send(err)
-        } else {
-            res.render('users/show', {
-                user: foundUser
-            })
-        }
-    })
-})
-
-
-router.delete('/:id', (req, res) => {
-    User.findOneAndDelete({ _id: req.params.id }, (err, deletedUser) => {
-        const userIds = [];
-        for (let i = 0; i < deletedUser.review.length; i++) {
-            userIds.push(deletedUser.review[i]._id)
-        }
-
-        Review.deleteMany(
-            {
-                _id: { $in: userIds }
-            },
-            (err, data) => res.redirect('/users')
-        )
-    })
-})
-
-
 
 module.exports = router
